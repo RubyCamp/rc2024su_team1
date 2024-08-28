22bc83821d67c73e9d4c43bd8d7bcfae9581ff3b
otomototomoni <tegurechafu150415@icloud.com>
2024-08-28 15:15:23 +0900

    sukosishusei

--- game_sample/scenes/game/card/No.2_yamatanooroti.rb
+++ game_sample/scenes/game/card/No.2_yamatanooroti.rb
@@ -4,7 +4,7 @@ module Scenes
       class Card2 < Base
         def initialize(x, y, z)
           self.imagesorce = "images/card_background.png"  # カードの画像を設定
-          self.illustimage = "images/matuecashtle.jpg"
+          self.illustimage = "images/card1.png"
           super(2, x, y, z) # Base クラスの initialize メソッドを呼び出す
           @suit = "Suit2"  # カードのスートを設定
           @effect_text = "you are death\nenemy is winner"  # カードの効果を設定

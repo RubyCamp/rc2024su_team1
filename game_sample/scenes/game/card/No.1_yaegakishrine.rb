f320ce5d1018e50d26e6769bdc964e30b76fa411
Hiroto Sano <prog.san234@gmail.com>
2024-08-28 14:24:22 +0900

    create_cardFile

---
+++ game_sample/scenes/game/card/No.1_yaegakishrine.rb
@@ -0,0 +1,14 @@
+module Scenes
+    module Game
+      module Card
+        class Card1 < Base
+          def initialize(x, y, z)
+            self.imagesorce = "images/card1.png"  # カードの画像を設定
+            super(1, x, y, z) # Base クラスの initialize メソッドを呼び出す
+            @suit = "Suit1"  # カードのスートを設定
+            @effect_text = "効果説明"  # カードの効果を設定
+          end
+        end
+      end
+    end
+  end

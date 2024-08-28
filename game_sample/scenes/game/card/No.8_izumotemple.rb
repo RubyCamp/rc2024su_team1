35c99055010421a1ac7d9a8ce2c31fab8014dd30
Nonomura Kanade <kanakana.nono1224@gmail.com>
2024-08-28 17:11:54 +0900

    card

--- game_sample/scenes/game/card/No.8_izumotemple.rb
+++ game_sample/scenes/game/card/No.8_izumotemple.rb
@@ -3,10 +3,31 @@ module Scenes
     module Card
       class Card8 < Base
         def initialize(x, y, z)
-          @imagesorce = "images/card1.png"  # カードの画像を設定
+          self.imagesorce = "images/card8.png"
           super(8, x, y, z) # Base クラスの initialize メソッドを呼び出す
           @suit = "Suit2"  # カードのスートを設定
           @effect_text = "効果説明"  # カードの効果を設定
+        end
+      end
+
+      class Effect8
+        attr_accessor :p1_wol, :p2_wol
+        def initialize(p1_deck, p2_deck, order, p1_wol, p2_wol)
+          @p1_deck = p1_deck
+          @p2_deck = p2_deck
+          @order = order
+          @p1_wol = p1_wol
+          @p2_wol = p2_wol
+        end
+
+        def judge
+          if @order == "p1"
+            @p1_wol = "L"
+            @p2_wol = "W"
+          else
+            @p1_wol = "W"
+            @p2_wol = "L"
+          end
         end
       end
     end
